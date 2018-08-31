8a5cc204258f8e85da28a1a5a600364be788d38e
Marcelo Zapatta <marcelo.zapatta@fatec.sp.gov.br>
2018-08-30 21:39:50 -0300

    Modal dos números já lançados

--- www/js/index.js
+++ www/js/index.js
@@ -168,8 +168,8 @@ var app = {
                 $('.btnHideContainer').html('<div class="d-flex p-2 justify-content-center"><a class="btnBaixoContainer align-self-center"><img src="img/btnBaixo.png"></a></div>');
                 $('.btnHideContainer').append('<div class="d-flex p-2 numeros-lancados flex-wrap"></div>');
 
-                $(numeros).each(function (index, value) {
-                    $('.numeros-lancados').append('<a class="button-numero-lancado" style="margin: 10px 10px 10px 10px; width: 25px;">' + value + '</a>');
+                $(chubs).each(function (index, value) {
+                    $('.numeros-lancados').append('<a class="button-numero-lancado" style="margin: 10px 10px 10px 10px; width: 33px;">' + value + '</a>');
                 });
 
                 $('.bingol-numero').hide();
@@ -257,115 +257,115 @@ var app = {
          * Valida se todos os numeros lançados correspondem
          * a cartela indicada nos campos
          */
-        function validarCampos() {
-            $('.bingol-cells').find('input').removeClass('bingol-success');
-            $('.bingol-cells').find('input').removeClass('bingol-error');
-            /**
-             * Armazena os números marcados da cartela
-             * já preenchida
-             * @type {Array}
-             */
-            var campos = [];
+        // function validarCampos() {
+        //     $('.bingol-cells').find('input').removeClass('bingol-success');
+        //     $('.bingol-cells').find('input').removeClass('bingol-error');
+        //     /**
+        //      * Armazena os números marcados da cartela
+        //      * já preenchida
+        //      * @type {Array}
+        //      */
+        //     var campos = [];
 
-            /**
-             * Variável de controle quando um número 
-             * está errado
-             */
-            var erro = { 
-                        i:null,
-                        valor:null 
-                        };
+        //     /**
+        //      * Variável de controle quando um número 
+        //      * está errado
+        //      */
+        //     var erro = { 
+        //                 i:null,
+        //                 valor:null 
+        //                 };
 
-            /**
-             * Array para conter todos os erros
-             */
-            var erros = [];
+        //     /**
+        //      * Array para conter todos os erros
+        //      */
+        //     var erros = [];
 
-            /**
-             * Variável de controle para pular a iteração
-             */
-            var skip = false;
+        //     /**
+        //      * Variável de controle para pular a iteração
+        //      */
+        //     var skip = false;
 
-            /**
-             * Variável de controle para pular todas as iterações
-             */
-            var skipAll = false;
+        //     /**
+        //      * Variável de controle para pular todas as iterações
+        //      */
+        //     var skipAll = false;
 
-            var verdadeiro = false;
+        //     var verdadeiro = false;
 
-            /**
-             * Percorre todas os campos da cartela
-             * armazenando no vetor campos
-             * @param  {int} index   da célula
-             */
-            $('.bingol-cell').each(function (index, celula) {
-                campos.push($(celula));
-            });
+        //     /**
+        //      * Percorre todas os campos da cartela
+        //      * armazenando no vetor campos
+        //      * @param  {int} index   da célula
+        //      */
+        //     $('.bingol-cell').each(function (index, celula) {
+        //         campos.push($(celula));
+        //     });
 
-            $(campos).each(function (indexCampo, campo) {
-                verdadeiro = false;
-                $(chubs).each(function (indexChubs, chub) {
+        //     $(campos).each(function (indexCampo, campo) {
+        //         verdadeiro = false;
+        //         $(chubs).each(function (indexChubs, chub) {
 
-                    // Verifica se é igual a algum elemento
-                    // presente em chubs e se o campo n eh verdade
-                    if(verdadeiro == false){
-                        if($(campo).val() == chub){
+        //             // Verifica se é igual a algum elemento
+        //             // presente em chubs e se o campo n eh verdade
+        //             if(verdadeiro == false){
+        //                 if($(campo).val() == chub){
 
-                            // Diz que o campo é verdadeiro
-                            verdadeiro = true;
-                        } else {
-                            if(indexChubs == chubs.length - 1){
-                                    erros.push({i:indexCampo, valor:$(campo).val()});
-                            }
-                        }
-                    }
-                })
-            })
+        //                     // Diz que o campo é verdadeiro
+        //                     verdadeiro = true;
+        //                 } else {
+        //                     if(indexChubs == chubs.length - 1){
+        //                             erros.push({i:indexCampo, valor:$(campo).val()});
+        //                     }
+        //                 }
+        //             }
+        //         })
+        //     })
 
-            if(erros.length !==  0) {
-                $('.bingol-cells').find('input').addClass('bingol-success');
+        //     if(erros.length !==  0) {
+        //         $('.bingol-cells').find('input').addClass('bingol-success');
 
-                $(erros).each(function (index, erro) {
-                    $('.bingol-cells').find('#'+erro.i).removeClass('bingol-success');
-                    $('.bingol-cells').find('#'+erro.i).addClass('bingol-error');
-                });
-            } else {
-                stop = false;
-                var audio = new Audio('assets/audio/tetra.mp3');
-                $('.bingol-cells').find('input').addClass('bingol-success');
-                $('#modalGanhou').modal('show');
+        //         $(erros).each(function (index, erro) {
+        //             $('.bingol-cells').find('#'+erro.i).removeClass('bingol-success');
+        //             $('.bingol-cells').find('#'+erro.i).addClass('bingol-error');
+        //         });
+        //     } else {
+        //         stop = false;
+        //         var audio = new Audio('assets/audio/tetra.mp3');
+        //         $('.bingol-cells').find('input').addClass('bingol-success');
+        //         $('#modalGanhou').modal('show');
 
-                $('#modalGanhou').on('hidden.bs.modal', function (e) {
-                    stop = true;
-                });
+        //         $('#modalGanhou').on('hidden.bs.modal', function (e) {
+        //             stop = true;
+        //         });
 
-                $('#modalGanhou').on('shown.bs.modal', function (e) {
-                    audio.play();
-                    fade();
-                });
+        //         $('#modalGanhou').on('shown.bs.modal', function (e) {
+        //             audio.play();
+        //             fade();
+        //         });
 
-                function fade() {
-                    if(!stop){
-                        $('#modalGanhou').fadeTo(600, 0.1, function () {
-                            if(!stop){
-                                $('#modalGanhou').fadeTo(600, 1.0 , fade($('#modalGanhou')));
-                            }
-                        });
-                    }
-                }
-            }
-        }
+        //         function fade() {
+        //             if(!stop){
+        //                 $('#modalGanhou').fadeTo(600, 0.1, function () {
+        //                     if(!stop){
+        //                         $('#modalGanhou').fadeTo(600, 1.0 , fade($('#modalGanhou')));
+        //                     }
+        //                 });
+        //             }
+        //         }
+        //     }
+        // }
 
         /**
          * Função para mostrar os numeros que já sairam
          * na tela de jogar
          * @param  Array numeros que já foram
          */
-        function atualizaNumeros(numeros) {
-            $(numeros).each(function (index, value) {
-                $('#numerosForam').append('<span class="btn btn-success numerinho">' + value + '</span>')
-            });
-        }
+        // function atualizaNumeros(numeros) {
+        //     $(numeros).each(function (index, value) {
+        //         $('#numerosForam').append('<span class="btn btn-success numerinho">' + value + '</span>')
+        //     });
+        // }
     }
 }
 
